1bc26847b4caae48df9ba2622a851ec8405311c6
sanketshevare <69853783+sanketshevare@users.noreply.github.com>
2022-05-23 18:38:35 +0530

    Update A1_TelephoneBook.cpp

--- A1_TelephoneBook.cpp
+++ A1_TelephoneBook.cpp
@@ -21,72 +21,49 @@ public:
         {
             ht[i].client_id = 0;
             ht[i].client_ph = 0;
-            
         }
     }
 
-int hash(long int key)
-{
-    return(key%max);
-}
+    int hash(long int key)
+    {
+        return (key % max);
+    }
 
     void insert()
     {
-        
+
         data d;
-	int iPos;
-	char cAns;
-        cout<<"Enter id"<<endl;
-        cin>>d.client_id;
-		cout<<"\n Enter Phone Number:";
-		cin>>d.client_ph;
-		iPos=hash(d.client_id);
-        cout<<iPos<<endl;
-	
-		if(ht[iPos].client_id==0)
-		{
-			ht[iPos]=d;
-		}
-		else
-		{
-			for(int i=iPos+1;i%max!=iPos;i++)
-			{
-				ht[i]=d;
-				break;
-			}
-		}
-	
-	
-}
+        int iPos;
+        char cAns;
+        cout << "Enter id" << endl;
+        cin >> d.client_id;
+        cout << "\n Enter Phone Number:";
+        cin >> d.client_ph;
+        iPos = hash(d.client_id);
+        cout << iPos << endl;
+
+        if (ht[iPos].client_id == 0)
+        {
+            ht[iPos] = d;
+        }
+        else
+        {
+            for (int i = iPos + 1; i % max != iPos; i++)
+            {
+                ht[i] = d;
+                break;
+            }
+        }
+    }
 
     void display()
     {
-        cout << "------------------------------------";
+
         cout << "\nSrno\tid\tPhone number\n";
-        cout << "------------------------------------\n";
+
         for (int i = 0; i < max; i++)
         {
             cout << i << "\t" << ht[i].client_id << "\t" << ht[i].client_ph << endl;
-        }
-        cout << "------------------------------------\n";
-    }
-
-    void search()
-    {
-        int x, flag;
-        cout << "Enter ph no to search: " << endl;
-        cin >> x;
-        for (int i = 0; i < max; i++)
-        {
-            if (ht[i].client_ph == x)
-            {
-                cout << "Client Phone Found At:-" << i << endl;
-                flag = 0;
-            }
-        }
-        if (flag == 1)
-        {
-            cout << "Client Phne not found!" << endl;
         }
     }
 
@@ -121,8 +98,7 @@ int main()
         cout << "0.EXIT" << endl
              << "1.INSERT" << endl
              << "2.DISPLAY" << endl
-             << "3.SEARCH" << endl
-             << "4.DELETE" << endl;
+             << "3.DELETE" << endl;
 
         cin >> ch;
 
@@ -135,10 +111,8 @@ int main()
         case 2:
             t.display();
             break;
+       
         case 3:
-            t.search();
-            break;
-        case 4:
             t.delete1();
             break;
         default:
