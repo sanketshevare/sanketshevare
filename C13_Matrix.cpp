06f0da4046a0fe5a3af8c4621d7e0e53c4d04bf3
sanketshevare <69853783+sanketshevare@users.noreply.github.com>
2022-05-23 19:26:00 +0530

    Update C13_Matrix.cpp

--- C13_Matrix.cpp
+++ C13_Matrix.cpp
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 using namespace std;
 
-int cost[10][10], i, j, k, n, qu[10], front, rear, v, visit[10], visited[10];
-int stk[10], top, visit1[10], visited1[10];
+int cost[10][10], i, j, k, n,v;
+
 
 int main()
 {
@@ -21,11 +21,10 @@ int main()
         cost[j][i] = 1;
     }
     
-    //display function
     cout << "The adjacency matrix of the graph is : " << endl;
-    for (i = 0; i < n; i++)
+    for (i = 0; i <= n; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = 0; j <= n; j++)
         {
             cout << " " << cost[i][j];
         }
@@ -36,44 +35,11 @@ int main()
     cin >> v;
     cout << "The BFS of the Graph is\n";
     cout << v<<endl;
-    visited[v] = 1;
-    k = 1;
-    while (k < n)
-    {
-        for (j = 1; j <= n; j++)
-            if (cost[v][j] != 0 && visited[j] != 1 && visit[j] != 1)
-            {
-                visit[j] = 1;
-                qu[rear++] = j;
-            }
-        v = qu[front++];
-        cout << v << " ";
-        k++;
-        visit[v] = 0;
-        visited[v] = 1;
-    }
-    
+  
     cout <<endl<<"Enter initial vertex : ";
     cin >> v;
     cout << "The DFS of the Graph is\n";
     cout << v<<endl;
-    visited[v] = 1;
-    k = 1;
-    while (k < n)
-    {
-        for (j = n; j >= 1; j--)
-            if (cost[v][j] != 0 && visited1[j] != 1 && visit1[j] != 1)
-            {
-                visit1[j] = 1;
-                stk[top] = j;
-                top++;
-            }
-        v = stk[--top];
-        cout << v << " ";
-        k++;
-        visit1[v] = 0;
-        visited1[v] = 1;
-    }
-
+   
     return 0;
 }
